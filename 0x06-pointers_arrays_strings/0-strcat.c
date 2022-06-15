93d53cd2c51c40a597848ff46f7b4495c40ad01e
Hayatudeen <hayatudeenabdulrahman@gmail.com>
2022-06-15 14:39:51 -0700

    Create 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _strcat - Concatenates 2 strings together
+* @dest: string 1
+* @src: string to be concatenated to string 1
+*
+* Return: on success, pointer to dest
+*/
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+	return (dest);
+}
