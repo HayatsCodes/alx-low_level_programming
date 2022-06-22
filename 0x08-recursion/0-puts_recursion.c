0a767de01b23f104166df108e27f8f9fc725d34c
Hayatudeen <hayatudeenabdulrahman@gmail.com>
2022-06-22 05:04:23 -0700

    fix typo

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _puts_recursion - Function prints a string, followed by a new line.
- * @s: String to be printed.
- */
+* _puts_recursion -Function prints a string, followed by a new line.
+* @s: String to be printed.
+*/
 void _puts_recursion(char *s)
 {
 	if (*s != '\0')
