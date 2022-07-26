9ee89db6afcd7863c6b9f31ff2e4cca58ef357ce
Hayatudeen <hayatudeenabdulrahman@gmail.com>
2022-07-26 04:15:40 -0700

    update betty doc

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,13 @@
 
 /**
 * add - add 2 numbers.
+* @a: first number
+* @b: second number
 *
 * Return: the sum of the numbers.
 */
 
 int add(int a, int b)
 {
-	return ( a + b);
+	return (a + b);
 }
